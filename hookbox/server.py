ddbc746a47e8d3a2ceb2ef2ed60c1d6c3944f3cf
Dan Gould <dlg@dangould.com>
2011-04-09 01:24:38 +0800

    WebSockWrapper also needs send_non_blocking

--- hookbox/server.py
+++ hookbox/server.py
@@ -377,12 +377,12 @@ class SockWebSocketWrapper(object):
             data = data.encode('utf-8')
         return data
 
-    def send(self, data):
-        self._ws.send(data)
+    def send(self, data, send_non_blocking=False):
+        self._ws.send(data, send_non_blocking=send_non_blocking)
         return len(data)
-        
-    def sendall(self, data):
-        self.send(data)
+
+    def sendall(self, data, send_non_blocking=False):
+        self.send(data, send_non_blocking=send_non_blocking)
         
     def __getattr__(self, key):
         return getattr(self._ws, key)
